a8ec7bb3512ca3dc890072d2fe5975c1da25a245
Vlad <vladicus7777720@mail.ru>
2020-11-24 13:23:56 +0300

    fix

--- main.cpp
+++ main.cpp
@@ -20,12 +20,8 @@ int main(int argc, char *argv[]) {
         if (!file.open(QIODevice::ReadOnly))
             throw std::runtime_error("Couldn't open save file.");
 
-        std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
         Graph *g = new Graph(QJsonDocument::fromJson(file.readAll()).object());
         g->calcCoords(16.0f / 9);
-        std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
-        std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << " [ms]" << std::endl;
-        w.setGraph(g);
     }
     catch (std::exception &e) {
         std::cout << e.what() << std::endl;
