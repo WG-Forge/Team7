d344eda1687fab83fb9d232af9a84d4f2ad2bbe9
BarikQ <yar.barik99@gmail.com>
2020-12-14 14:35:38 +0300

    fix: changed main ui props

--- gui/mainwindow.cpp
+++ gui/mainwindow.cpp
@@ -4,6 +4,7 @@
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow) {
     ui->setupUi(this);
     ui->graphview->hide();
+    ui->postInfo->hide();
 }
 
 MainWindow::~MainWindow() {
